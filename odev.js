1aa347faae5250f863924478dfa0144e79504c2d
kenansahin <kenansahin803@gmail.com>
2021-02-24 21:56:07 +0200

    nonbackground

---
+++ odev.js
@@ -0,0 +1,17 @@
+const numbers = [0,1,2,3,4,5,6,7,8,9];
+const opetors = ['+,-,/,*,=,c'];
+
+window.onload = function () {
+var numbers = document.querySelectorAll(".num");
+    for (var i = 0; i < numbers.length; i++);
+        numbers [i].addEventListener('click', function(){
+        }, false);
+},
+}
+window.onload = function () {
+    var opertors = document.querySelectorAll(".operator");
+        for (var i = 0; i < opertors.length; i++);
+            numbers [i].addEventListener('click', function(){
+            }, false);
+    }
+    
